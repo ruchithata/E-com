31ea990b86ee29ce026f92fe84000473c8dcd0f7
T A Ruchitha <ruchitha.ta83@gmail.com>
2025-02-22 19:24:02 +0530

    +

--- Backend/src/Utils/Errorhandler.js
+++ Backend/src/Utils/Errorhandler.js
@@ -1,7 +1,7 @@
 class Errorhandler extends Error {
     constructor(statusCode,message) {
         super(message);
-        this.statusCode - statusCode;
+        this.statusCode = statusCode;
     }
 }
 
